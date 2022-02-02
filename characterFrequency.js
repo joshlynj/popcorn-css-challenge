16969bd7d42d8dbf57caa9afbafbdbfce96021b5
joshlynj <joshlyn294@gmail.com>
2022-02-02 08:12:00 -0700

    final

---
+++ characterFrequency.js
@@ -0,0 +1,12 @@
+var characterFrequency = function (string) {
+    let freq = {} ;
+    let resultArray = [];
+    // for loop to iterate through string input
+    for (let i = 0; i < string.length; i++) {
+        let char = string.index(i)
+        // if statement to see if char already is exists
+        if (freq[char]){
+            
+        }
+    }
+  };
